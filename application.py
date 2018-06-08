3eea0237f1f01fcdc22daabbd11b7d704a6e63b2
pavan16pa1a0563 <38848720+pavan16pa1a0563@users.noreply.github.com>
2018-06-08 16:48:18 +0530

    Update application.py

--- application.py
+++ application.py
@@ -375,4 +375,4 @@ def showCategoryItemJSON(catalog_id, item_id):
 if __name__ == '__main__':
 	app.debug = True
 	app.secret_key = 'super_secret_key'
-	app.run(host = '0.0.0.0', port = 7000)
+	app.run(host = '0.0.0.0', port = 5000)
